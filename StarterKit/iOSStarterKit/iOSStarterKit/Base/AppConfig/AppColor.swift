689824bd3d03404234458c0338b3e6944c7ed52c
MonsterTechStudio <martinvegance@yahoo.com>
2018-08-20 13:57:55 +0800

    add app files

---
+++ StarterKit/iOSStarterKit/iOSStarterKit/Base/AppConfig/AppColor.swift
@@ -0,0 +1,9 @@
+//
+//  AppColor.swift
+//  iOSStarterKit
+//
+//  Created by xiongyongjie on 2018/8/20.
+//  Copyright © 2018 MonsterTech. All rights reserved.
+//
+
+import Foundation
---
+++ StarterKit/iOSStarterKit/iOSStarterKit/Base/AppConfig/AppFont.swift
@@ -0,0 +1,9 @@
+//
+//  AppFont.swift
+//  iOSStarterKit
+//
+//  Created by xiongyongjie on 2018/8/20.
+//  Copyright © 2018 MonsterTech. All rights reserved.
+//
+
+import Foundation
